09081def50c5148459c92557a599d5d9eaa9c522
Natanim Ashenafi Kefeni <142803189+natanim-ashenafi@users.noreply.github.com>
2024-01-02 17:54:53 +0300

    Fixed home page (#19)
    Co-authored-by: natanim-ashenafi <natanim-ashenafi@a2sv.org>

--- Web/contest-central/src/app/_components/Drawer/MiniDrawerStyled.js
+++ Web/contest-central/src/app/_components/Drawer/MiniDrawerStyled.js
@@ -14,7 +14,7 @@ const openedMixin = (theme) => ({
   }),
   overflowX: 'hidden',
   boxShadow: 'none'
-});
+}); 
 
 const closedMixin = (theme) => ({
   transition: theme.transitions.create('width', {
--- Web/contest-central/src/app/_components/TotalCards.tsx
+++ Web/contest-central/src/app/_components/TotalCards.tsx
@@ -4,9 +4,9 @@ import totalCandidates from '../../../public/totalCandidates.png'
 
 const TotalCards = () => {
   return (
-    <div className='flex w-80 h-auto shadow-lg shadow-gray-400 rounded-2xl py-3 px-3 gap-x-3 bg-white'>
+    <div className='flex h-auto shadow-lg shadow-gray-400 rounded-2xl py-3 pl-3 pr-20 gap-x-3 bg-white'>
       <Image src={totalCandidates} alt='alt' className='' /> 
-      <div className='text-xl text-primaryGray-400'>
+      <div className='text-lg text-primaryGray-400'>
         <p>Total Contest</p>
         <p className='text-black font-semibold'>200</p>
       </div>
