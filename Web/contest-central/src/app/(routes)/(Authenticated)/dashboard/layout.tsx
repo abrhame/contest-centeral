6f13ddc89d322aa36b08eab2272e08131214d9c0
jamyMarkos <jamymarcoss47@gmail.com>
2024-01-10 18:33:39 +0300

    aastu.web3.mintesnot-axios-setup

--- Web/contest-central/src/app/(routes)/(Authenticated)/dashboard/layout.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-
-import { useState } from "react";
-import { IoMdClose } from "react-icons/io";
-
-export default function LeaderBoardLayout(props: React.PropsWithChildren) {
-  return <div className="bg-primaryGray-100 h-screen">{props.children}</div>;
-}
